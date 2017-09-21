abfcb525fc99e7e2bcb44a3889a77cf00cb32690
Low Jian Sheng <jiansheng@clarifai.com>
2017-09-21 16:50:39 -0400

    final reformat

--- scrape_scholar.py
+++ scrape_scholar.py
@@ -1,11 +1,40 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-import time
+import subprocess
 
-def init_driver():
-    driver = webdriver.Chrome()
-    driver.wait = WebDriverWait(driver, 5)
-    return driver
+def run_scrape(list_of_keywords):
+    authors_to_search = {}
+    for keyword in list_of_keywords:
+        bashCommand = "python scholar.py -A " + keyword + " -c 10 --citation=en"
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()   
+        authors = parse_output(keyword, output)
+        authors_to_search[keyword] = authors
+    print(authors_to_search)
+    return authors_to_search
 
-if __name__ == "__main__":
-    driver = init_driver()
+def parse_output(keyword, output):
+    articles = output.split('\n\n')
+    authors = []
+    for article in articles:
+        authors_pre_processed = article.split("%A ")
+        num_authors = len(authors_pre_processed)
+        for i, author in enumerate(authors_pre_processed):
+            # ignore the first and last indices as they are not authors
+            if i == 0:
+                continue
+            # the last index needs to further process
+            if i == num_authors - 1:
+                last_author = author.split("%")
+                author = last_author[0]
+                author = author[:-4]
+                authors.append(author)
+                break
+            author = author[:-4]
+            authors.append(author)
+    return authors
+
+def main():
+    user_input = raw_input("Input keywords:(delimited by ;) ")
+    list_of_keywords = user_input.split(";")
+    run_scrape(list_of_keywords)
+
+main()
